ddf4e6a2b309a5526a9a8cbd1871bf172128a653
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-28 15:43:55 -0500

    timeout push

---
+++ index.js
@@ -0,0 +1,72 @@
+//import our customers
+const customers = require("./customers.json");
+
+// //import a set of tools to talk to firebase and firestore
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+
+// import our credentials
+const credentials = require("./credentials.json");
+
+//connect to firebase services
+initializeApp({
+  credential: cert(credentials),
+});
+
+// //connect to firestore
+const db = getFirestore();
+console.log("it worked");
+
+// // refernce to restaurant collect since used heavily
+// const restRef = db.collection("restaurants");
+
+//create a collection called "restaurants"
+
+//add each restaurants
+// restRef
+//   .add(restaurants[1])
+//   .then((doc) => {
+//     console.log("added restaurant", doc.id);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//read one document
+// db.collection(restaurants)
+//   .doc("rMPMYE1RhsOmmYQKIkcr")
+//   .get()
+//   .then((doc) => {
+//     console.log(doc.id, " => ", doc.data());
+//   })
+//   .catch((err) => console.error(err));
+
+//get all documents..below we actually get a snapshot of the database at that time.
+// restRef
+//   .get()
+//   .then((snapshot) =>
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, " => ", doc.data());
+//     })
+//   )
+//   .catch(console.error);
+
+//querying a collection
+// restRef
+//   .where("name", "==", "Bolay")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       //todd may not have had snapshot on this line
+//       console.log(doc.data());
+//     });
+//   })
+//   .catch(console.error);
